9b10ea93bb53ed5e02e158f83958756ebdea866f
Sohaghori839 <sohaghori839@gmail.com>
2023-05-28 01:51:46 +0500

    responsive

---
+++ orders.js
@@ -0,0 +1,14 @@
+const Orders = {
+    {
+        productName; "Foldable Mini Drone",
+        productNumber; '85631',
+        paymentStatus;'Due',
+        shipping; 'Pending'
+    },
+    {
+        productName; "Lamintor Drone",
+        productNumber; '95632',
+        paymentStatus;'Refunded';
+        shipping; "Declined"
+    },
+}
